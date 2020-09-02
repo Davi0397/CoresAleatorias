2593deed87ab52b20c9e5d0e5f317ca2341724de
Davi0397 <53841230+Davi0397@users.noreply.github.com>
2020-09-02 09:07:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 		//Definindo o elemento que irá mudar de cor pelo seu ID
 
 		var p3 = Math.floor(Math.random() * 240);//definindo os valores que serão gerados aleatoriamente
-		var p4 = Math.floor(Math.random() * 240);//definindo os valores que serão gerados aleatoriamente
-		var p5 = Math.floor(Math.random() * 240);//definindo os valores que serão gerados aleatoriamente
+		var p4 = Math.floor(Math.random() * 240);//240 é o número máximo para definir uma cor em RGB
+		var p5 = Math.floor(Math.random() * 240);//Meth.floor é para arredondar o número
 
 
 		ElementoHTML.style.background = "rgb("+p3+","+p4+","+p5+")";
